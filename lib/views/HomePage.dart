f16234b57c90e21564b61b599e6018c73749d470
Clément VEY <78201543+CoderNerfer@users.noreply.github.com>
2022-06-02 22:15:27 +0200

    :sparkles: Build: Finish Home Page UI

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +22,126 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.only(top: 30.0),
           child: Column(children: [
-            const Text('HAHA homepage'),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Next'),
+            const Text(
+              "PARIS",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 144, 168, 253),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(children: [
+                          const Text(
+                            "Monday\n02, 2022\n21:00",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 15.0),
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              color: Colors.black,
+                            ),
+                            // Image(
+                            //   image: NetworkImage(
+                            //       "http://openweathermap.org/img/wn/10d.png"),
+                            // )
+                          ),
+                        ]),
+                        Column(children: const [
+                          Text(
+                            "10,01°",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Humidity: 40%\nWind: 5,02 kmh",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ]),
+                      ]),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Container(
+                height: 400,
+                child: ListView.builder(
+                  itemCount: 24,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 5.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 144, 168, 253),
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 10.0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  "11:00",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  color: Colors.black,
+                                ),
+                                // Image(
+                                //   image: NetworkImage(
+                                //       "http://openweathermap.org/img/wn/10d.png"),
+                                // )
+                                const Text(
+                                  "12,03°",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ]),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
           ]),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(255, 144, 168, 253),
+        child: const Icon(Icons.add),
       ),
     );
   }
