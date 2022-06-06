97907ef55656dd1aef5f9de150d8c6529cbd2bd4
Lixlechat <effy91@me.com>
2022-06-06 17:44:58 +0200

    API+SQFLITE

--- lib/data/bdd.dart
+++ lib/data/bdd.dart
@@ -1,1 +1,75 @@
-import 'package:sqflite/sqflite.dart';
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLRequest {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE cities(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        city TEXT,
+        latitude TEXT,
+        longitude TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      """);
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'ynov.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  // Create new city
+  static Future<int> createCity(
+      String city, String latitude, String longitude) async {
+    final db = await SQLRequest.db();
+
+    final data = {'city': city, 'latitude': latitude, 'longitude': longitude};
+    final id = await db.insert('cities', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+
+  // Fetch all cities
+  static Future<List<Map<String, dynamic>>> getCities() async {
+    final db = await SQLRequest.db();
+    return db.query('cities', orderBy: "id");
+  }
+
+  // Read a single city by id
+  static Future<List<Map<String, dynamic>>> getCity(int id) async {
+    final db = await SQLRequest.db();
+    return db.query('cities', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  // Update a city by id
+  static Future<int> updateCity(
+      int id, String city, String latitude, String longitude) async {
+    final db = await SQLRequest.db();
+
+    final data = {
+      'city': city,
+      'latitude': latitude,
+      'longitude': longitude,
+      'createdAt': DateTime.now().toString()
+    };
+
+    final result =
+        await db.update('cities', data, where: "id = ?", whereArgs: [id]);
+    return result;
+  }
+
+  // Delete
+  static Future<void> deleteCity(int id) async {
+    final db = await SQLRequest.db();
+    try {
+      await db.delete("cities", where: "id = ?", whereArgs: [id]);
+    } catch (err) {
+      debugPrint("Something went wrong when deleting a city: $err");
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ynov_meteo/model/city.dart';
 
 void main() {
   runApp(const MyApp());
