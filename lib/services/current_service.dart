6ab6ea708011bf177b141079e6756793b8f0d655
Clément VEY <78201543+CoderNerfer@users.noreply.github.com>
2022-06-07 12:03:04 +0200

    :tada: build: V1 home UI and API

---
+++ lib/services/current_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:ynov_meteo/model/current.dart';
+import 'package:http/http.dart' as http;
+
+Future<Current> getCurrentData() async {
+  Current current = Current(
+    Coord(0.00, 0.00),
+    [Weather("", "", "")],
+    Main(0.00, 0),
+    Wind(0.00),
+    0,
+    0,
+  );
+
+  var url = Uri.https("api.openweathermap.org", '/data/2.5/weather',
+      {'q': 'Lyon', 'appid': '4364afa41febe0e86a2e4b3ae7860fed'});
+  var responce = await http.get(url);
+  if (responce.statusCode == 200) {
+    var jsonResponse = jsonDecode(responce.body);
+    current = Current(
+      Coord(jsonResponse['coord']['lon'], jsonResponse['coord']['lat']),
+      [
+        Weather(
+            jsonResponse['weather'][0]['main'],
+            jsonResponse['weather'][0]['description'],
+            jsonResponse['weather'][0]['icon']),
+      ],
+      Main(jsonResponse['main']['temp'], jsonResponse['main']['humidity']),
+      Wind(jsonResponse['wind']['speed']),
+      jsonResponse['dt'],
+      jsonResponse['timezone'],
+    );
+  } else {
+    print('Request failed : ${responce.statusCode}');
+  }
+  return current;
+}
--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ynov_meteo/views/NavBar.dart';
+import 'package:ynov_meteo/model/current.dart';
+import 'package:ynov_meteo/services/current_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -26,52 +28,65 @@ class _HomePage extends State<HomePage> {
           padding: const EdgeInsets.only(top: 30.0),
           child: Column(children: [
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 20.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 144, 168, 253),
-                  borderRadius: BorderRadius.circular(12),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 30.0, horizontal: 20.0),
+                child: FutureBuilder<Current>(
+                  future: getCurrentData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                          child: Text("Chargement en cours ..."));
+                    } else {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 144, 168, 253),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(children: [
+                                  const Text(
+                                    "Monday\n02, 2022\n21:00",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 15.0),
+                                    child: Image(
+                                      image: NetworkImage(
+                                          "http://openweathermap.org/img/wn/" +
+                                              snapshot.data!.weather[0].icon +
+                                              ".png"),
+                                    ),
+                                  ),
+                                ]),
+                                Column(children: [
+                                  Text(
+                                    "${snapshot.data!.main.temp - 273.15}",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Humidite: ${snapshot.data!.main.humidity}%\nVent: ${snapshot.data!.wind.speed}kmh",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ]),
+                              ]),
+                        ),
+                      );
+                    }
+                  },
+                )
+                //
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(children: const [
-                          Text(
-                            "Monday\n02, 2022\n21:00",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 15.0),
-                            child: Image(
-                              image: NetworkImage(
-                                  "http://openweathermap.org/img/wn/10d.png"),
-                            ),
-                          ),
-                        ]),
-                        Column(children: const [
-                          Text(
-                            "10,01°",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                          ),
-                          Text(
-                            "Humidity: 40%\nWind: 5,02 kmh",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ]),
-                      ]),
-                ),
-              ),
-            ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
