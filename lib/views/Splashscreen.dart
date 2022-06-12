1801fe4b7fd87c5ee9b9745d7e3fe0706d4eb02d
Clément VEY <78201543+CoderNerfer@users.noreply.github.com>
2022-06-12 21:29:55 +0200

    :tada: BUILD: splashscreen work

---
+++ lib/views/Splashscreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:ynov_meteo/views/CitiesManagement.dart';
+import 'package:page_transition/page_transition.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: 'assets/logo_icon.png',
+      nextScreen: CitiesManagement(title: ' '),
+      splashTransition: SplashTransition.slideTransition,
+      pageTransitionType: PageTransitionType.bottomToTop,
+      backgroundColor: Colors.blue,
+    );
+  }
+}
